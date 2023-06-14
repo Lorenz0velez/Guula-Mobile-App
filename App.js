0d196580374c3b93ba3f8cf5dfdeba19473a849b
Lorenz0velez <lorenzoveleznavarro@gmail.com>
2023-06-14 11:32:02 -0300

    first-commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.child_container}>
+      <Text>Este es un Text</Text>
+      <StatusBar style="auto" />
+    </View>
+    <View style={styles.child_container}>
+      <Text>Este es otro text</Text>
+      <StatusBar style="auto" />
+    </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    color: "red",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  child_container: {
+
+  },
+});

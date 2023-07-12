6a0da80b915f2c3ceadfb8d2b6b2711f96a3ee69
Lorenz0velez <lorenzoveleznavarro@gmail.com>
2023-07-12 12:04:54 -0300

    listo el getByName

--- api/src/controllers/getRestaurantsByName.js
+++ api/src/controllers/getRestaurantsByName.js
@@ -1,63 +1,27 @@
 const { Bar } = require("../db");
 const { Op } = require('sequelize');
 
-// const getRestaurantsByName = async (barName) => {
-//   const restaurants = await Bar.findAll({
-//     where: {
-//       name: {
-//         // [Op.iLike]: `%${barName}%`, // Utiliza Op.iLike para hacer una búsqueda sin distinguir mayúsculas y minúsculas
-//         [Op.like]: `%${barName}%`,
-//       },
-//       active: true,
-//     },
-//   });
-//   if (restaurants.length === 0) {throw new Error('No matches found');}
-//   return restaurants;
-// };
-// module.exports = getRestaurantsByName;
+// barRouter.get('/restaurantes/:nombre', async (req, res) => { ---->>>>>>>>>> ESTO ME LO DIO CHATGPT
+//   const name = req.params.nombre;
 
+//   try {
+//     // Buscar el restaurante por su nombre en la base de datos
+//     const restaurante = await Bar.findOne({ where: { name } });
 
-// const getRestaurantsByName = async (barName) =>{
-//     //
-//     let search = barName.toLowerCase().split(" ")
-//     for (let i = 0; i < search.length; i++) {
-//         search[i] = search[i][0].toUpperCase() + search[i].substr(1);
+//     if (restaurante) {
+//       res.json(restaurante);
+//     } else {
+//       res.status(404).json({ error: 'Restaurante no encontrado' });
 //     }
-//     let searchBar = search.join(" ");
-//     const restaurants = await Bar.findAll({
-//         where: {
-//             title : {
-//             [Op.like] : `%${searchBar}%`
-//             },
-//             active: true
-//         }
-//     })
-//     //
+//   } catch (error) {
+//     console.error('Error al buscar el restaurante:', error);
+//     res.status(500).json({ error: 'Error interno del servidor' });
+//   }
+// });
 
-//     if(!restaurants) throw new Error ('No matches found')
-
-//     return restaurants;
-// }
-const getRestaurantsByName = async (barName) =>{
-    
-    const restaurants = await Bar.findAll({})
-    //
-    if (barName) {
-    return await restaurants.filter((dog) =>
-      dog.name.trim().toLowerCase().includes(barName.trim().toLowerCase())
-    );
-  }
-
-    if(!restaurants) throw new Error ('No matches found')
-
-    // return restaurants;
-    return console.log('el nombre buscado es: ', barName)
+const getRestaurantsByName = async(name)=>{
+  const barcito = await Bar.findOne({ where: { name } });
+  return barcito;
 }
 
-module.exports = getRestaurantsByName;
-
-// if (name) {
-//     return await dbDogs.filter((dog) =>
-//       dog.name.trim().toLowerCase().includes(name.trim().toLowerCase())
-//     );
-//   }
+module.exports= getRestaurantsByName;
--- api/src/routes/barRouter.js
+++ api/src/routes/barRouter.js
@@ -1,5 +1,7 @@
 const { Router } = require('express');
 const getAllRestaurants = require('../controllers/getAllRestaurants');
+// const getRestaurantsByName = require('../controllers/getRestaurantsByName');
+const { Bar } = require("../db");
 const getRestaurantsByName = require('../controllers/getRestaurantsByName');
 
 
@@ -19,28 +21,24 @@ barRouter.get('/bares', async(req,res,next)=>{
         console.log(error)
         return res.status(400).json({error:error.message})
     } 
-})
+});
 
-// barRouter.get('/', async (req, res) => {
-//     const { barName } = req.query;
-
-    // if (barName) {
-    //     try {
-    //         const barcito = await getRestaurantsByName(barName)
-    //         return res.status(200).json(barcito) 
-    //     } catch (error) {
-    //         return res.status(400).json({error:error.message}) 
-    //     }
-    // }
-
-    // try {
-    //     const allRestaurants = await getAllRestaurants();
-    //     return res.status(200).json(allRestaurants);
-    // } catch (error) {
-    //     console.log('error:', error);
-    //     return res.status(400).json({error:error.message})
-    // } 
-
-// });
+barRouter.get('/restaurantes/:nombre', async (req, res) => {
+    const name = req.params.nombre;
+  
+    try {
+      // Buscar el restaurante por su nombre en la base de datos
+      const restaurante = await getRestaurantsByName(name);
+  
+      if (restaurante) {
+        res.json(restaurante);
+      } else {
+        res.status(404).json({ error: 'Restaurante no encontrado' });
+      }
+    } catch (error) {
+      console.error('Error al buscar el restaurante:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+    }
+  });
 
 module.exports = barRouter;
